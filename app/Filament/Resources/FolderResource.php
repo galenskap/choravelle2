43e29ac1237c3524ab699626b008587260dd95a1
galenskap <jean.deborah@gmail.com>
2025-02-25 16:23:15 +0100

    Pagination classeurs

--- app/Filament/Resources/FolderResource.php
+++ app/Filament/Resources/FolderResource.php
@@ -73,7 +73,7 @@ class FolderResource extends Resource
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ])
-            ->defaultPaginationPageOption('all');
+            ->defaultPaginationPageOption(100);
     }
 
     public static function getRelations(): array
