1e382a47a259d6774df9c322c454ab3594ea83cf
galenskap <jean.deborah@gmail.com>
2025-05-22 09:58:39 +0200

    Fix display of account submenu - desktop

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,9 +18,9 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6 dropdown">
                 @auth
-                    <x-dropdown align="right" width="48">
+                    <x-dropdown align="right" width="w-72">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->name }}</div>
