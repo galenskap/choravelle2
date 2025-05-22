23376f78af77b6be3c1767bd3e8b1c8f1179526d
galenskap <jean.deborah@gmail.com>
2025-05-22 13:46:33 +0200

    New page default layout - fix

--- resources/views/cms/page.blade.php
+++ resources/views/cms/page.blade.php
@@ -2,12 +2,23 @@
 
 @section('content')
 
-    @if (!$page->isHomepage())
-    <h1 class="page-title">{{ $page->title }}</h1>
-    @endif
+@if (!$page->isHomepage())
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="p-6 text-gray-900">
+            <h1 class="page-title text-2xl font-bold">{{ $page->title }}</h1>
+            
+            @foreach ($blocks as $block)
+                @include('cms.blocks.' . $block->getTemplateCode(), ['content' => $block->content, 'slug' => $block->slug])
+            @endforeach
+        </div>
+    </div>
+</div>
 
+@else 
     @foreach ($blocks as $block)
         @include('cms.blocks.' . $block->getTemplateCode(), ['content' => $block->content, 'slug' => $block->slug])
     @endforeach
+@endif
 
 @endsection
